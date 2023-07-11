d0d7344a422d2f7b724434b1a3deb6d75c70326b
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-07-11 11:45:54 +0530

    Add project files.

---
+++ InterfacesInCollections/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesInCollections
+{
+    public class Player
+    {
+        private int runs;
+        private string names;
+
+
+        public Player(int runs, string names)
+        {
+            this.runs = runs;
+            this.names = names;
+        }
+
+        public override string ToString()
+        {
+            return $"{runs}-----------{names}";
+        }
+    }
+
+
+
+    public class Team : IEnumerable//When we have collections we need to call IEnumerable
+    {
+
+        Player[] players;   //Class array
+        public Team()
+        {
+            players = new Player[3];
+            players[0] = new Player(1, "Virat");
+            players[1] = new Player(2, "Sachin");
+            players[2] = new Player(3, "Dhoni");
+        }
+        public IEnumerator GetEnumerator()
+        {
+           return players.GetEnumerator();
+        }
+    }
+}
